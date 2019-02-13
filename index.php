bd629f3eb89215e15de9668ef6770c7d0d5da66a
jashan11 <45364040+jashan11@users.noreply.github.com>
2019-02-13 22:51:10 +0530

    Added Nav Bar
    Please check the responsiveness

--- index.php
+++ index.php
@@ -6,9 +6,19 @@
     <head>
     	<title>Olive Ivory</title>
         <link rel="stylesheet" href="home.css"/>
-    	
+    	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="./nav.css">
     </head>
-    <body >
-
+    <body>
+<div class="topnav" id="myTopnav">
+  <a href="#home" class="active">Home</a>
+  <a href="#news">News</a>
+  <a href="#contact">Contact</a>
+  <a href="#about">About</a>
+  <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+    <i class="fa fa-bars"></i>
+  </a>
+</div>
+<script type="text/javascript src="navv.js"></script>
     </body>
 </html>
