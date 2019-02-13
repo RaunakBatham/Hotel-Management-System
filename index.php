9ef62463d273fefbb6695b1b8c9230d8bf8cf95a
Raunak <007raunakbatham@gmail.com>
2019-02-13 22:24:05 +0530

    Started working on Homepage

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
 // Homepage for the website.
 ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Olive Ivory</title>
+        <link rel="stylesheet" href="home.css"/>
+    </head>
+    <body>
+
+    </body>
+</html>
